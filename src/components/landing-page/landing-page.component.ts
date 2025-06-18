d4be14a33bf58c23243d14795f0ed0930f7cbebc
zagyvatamas <xnoszix@gmail.com>
2025-06-18 20:18:23 +0200

    landing page header ready and profil image implement

--- src/components/landing-page/landing-page.component.ts
+++ src/components/landing-page/landing-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
+  standalone:true,
   imports: [],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
