129f5c7a1200f1dac4ae9d8ef27a3156dd56a612
zagyvatamas <xnoszix@gmail.com>
2025-06-19 23:17:46 +0200

    prices and reservation-pending component create and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,8 @@ import { ProfileComponent } from '../components/profile/profile.component';
 import { LandingPageComponent } from '../components/landing-page/landing-page.component';
 import { NavbarComponent } from '../components/navbar/navbar.component';
 import { ReservationComponent } from '../components/reservation/reservation.component';
+import { PricesComponent } from '../components/prices/prices.component';
+import { ReservationPendingComponent } from '../components/reservation-pending/reservation-pending.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent},
@@ -14,5 +16,7 @@ export const routes: Routes = [
     { path: '', redirectTo: 'landing', pathMatch: 'full' },
     {path: 'landing', component:LandingPageComponent},
     {path: 'navbar', component:NavbarComponent},
-    {path: 'reservation', component:ReservationComponent}
+    {path: 'reservation', component:ReservationComponent},
+    {path: 'prices', component:PricesComponent},
+    {path: 'reservationPending', component:ReservationPendingComponent}
 ];
---
+++ src/components/reservation-pending/reservation-pending.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reservation-pending',
+  imports: [],
+  templateUrl: './reservation-pending.component.html',
+  styleUrl: './reservation-pending.component.css'
+})
+export class ReservationPendingComponent {
+
+}
